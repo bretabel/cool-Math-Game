152ad97be747cece77a884681ffb29aa2dee0bf2
Ellis Collison <elliscollison1@gmail.com>
2019-10-16 15:02:30 -0700

    Adds math expression functionality

---
+++ Horse Game/src/horsegame/Expression.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class Expression {
+	int op1, op2;
+	char operator;
+	
+	public Expression() {
+		Random rng = new Random();
+		
+		op1 = rng.nextInt(11);
+		int temp = rng.nextInt(1);
+		
+		if (temp == 0) {
+			operator = '-';
+		} else {
+			operator = '+'; 
+		}
+		
+		if (operator == '-') {
+			op2 = rng.nextInt(op1 + 1);
+		} else {
+			op2 = rng.nextInt(11);
+		}
+	}
+	
+	public Expression(char operator) {
+		Random rng = new Random();
+		if (operator == '-') {
+			op1 = rng.nextInt(11);
+			op2 = rng.nextInt(op1 + 1);
+		} else {
+			op1 = rng.nextInt(11);
+			op2 = rng.nextInt(11);
+		}
+		this.operator = operator; 
+	}
+	
+	public Expression(String expression) {
+		String[] split = expression.split(" ");
+		
+		try {
+			op1 = Integer.parseInt(split[0]);
+			op2 = Integer.parseInt(split[2]);
+			operator = split[1].charAt(0);
+		} catch (Exception e) {
+			System.out.println("Invalid expression entered.");
+		}
+	}
+	
+	public Expression(int op1, int op2, char operator) {
+		this.op1 = op1;
+		this.op2 = op2;
+		this.operator = operator;
+	}
+	
+	public int getSolution() {
+		if (operator == '+') {
+			return op1 + op2;
+		} else {
+			return op1 - op2;
+		}
+	} 
+	
+	public Boolean isSolution(int solution) {
+		if (solution == getSolution()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		return op1 + " " + operator + " " + op2;
+	}
+	
+	public String solutionString() {
+		return toString() + " = " + getSolution();
+	}
+	
+	public Boolean equals(Expression other) {
+		if(other.op1 == this.op1 && other.op2 == this.op2 && other.operator == this.operator){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ Horse Game/src/horsegame/ProblemSet.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+
+public class ProblemSet {
+	ArrayList<Expression> problemList = new ArrayList<Expression>();
+	
+	/**
+	 * Randomly populates list with 10 problems
+	 */
+	public ProblemSet() {
+		for(int i = 0; i < 10; i++) {
+			Expression newExpression = new Expression();
+			problemList.add(newExpression);
+		}
+	}
+	
+	/**
+	 * Randomly populates list with problems of either addition [exclusive] or subtraction
+	 * @param operator either + or - specifying what kind of expression to populate the list
+	 */
+	public ProblemSet(char operator) {
+		if (operator == '-') {
+			for(int i = 0; i < 10; i++) {
+				Expression newExpression = new Expression('-');
+				problemList.add(newExpression);
+			}
+		} else {
+			for(int i = 0; i < 10; i++) {
+				Expression newExpression = new Expression('+');
+				problemList.add(newExpression);
+			}
+		}
+	}
+	
+	/**
+	 * Randomly populates list with inputed num of problems
+	 * @param numProblems number of problems to include in list
+	 */
+	public ProblemSet(int numProblems) {
+		for(int i = 0; i < numProblems; i++) {
+			Expression newExpression = new Expression();
+			problemList.add(newExpression);
+		}
+	}
+	
+	public void addExpression(Expression e) {
+		if(!(problemList.contains(e))) {
+			problemList.add(e);
+		}
+	}
+	
+	public Expression getFirst() {
+		Expression e = problemList.get(0);
+		problemList.remove(0);
+		return e;
+	}
+	
+	public Expression getRandom() {
+		Random rng = new Random(); 
+		int randomIndex = rng.nextInt(problemList.size());
+		Expression e = problemList.get(randomIndex);
+		problemList.remove(randomIndex);
+		return e;
+	}
+	
+}
